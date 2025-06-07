ab108330b8ead3a65dd20fb74e59fa5d0df052b1
Ralph Maron Eda <edaralphmaron@gmail.com>
2025-06-07 20:33:10 +0800

    initial commit

---
+++ src/test/kotlin/com/ralphmarondev/note/NoteApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ralphmarondev.note
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class NoteApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
