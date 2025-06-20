56463f2de24201d67e60fc070026842e5aa86c50
Ralph Maron Eda <edaralphmaron@gmail.com>
2025-06-20 23:22:41 +0800

    client: read and display note from api

--- client/src/views/home/note-list/NoteListIndex.vue
+++ client/src/views/home/note-list/NoteListIndex.vue
@@ -1,9 +1,48 @@
 <script setup lang="ts">
+import NoteCard from "@/views/home/note-list/components/NoteCard.vue";
+import {onMounted, ref} from "vue";
+import axiosInstance from "@/axiosInstance.ts";
+
+type Note = {
+  id: number
+  title: string
+  caption: string
+  createDate: string
+}
+
+const notes = ref<Note[]>([])
+
+onMounted(async () => {
+  try {
+    const response = await axiosInstance.get("/notes", {
+      params: {
+        ownerId: 1
+      }
+    })
+    notes.value = response.data
+  } catch (err) {
+    console.error('Failed to load notes: ', err)
+  }
+})
 
 </script>
 
 <template>
-  <section class="h-screen w-screen flex items-center justify-center bg-pink-50">
-    <h3 class="text-2xl text-pink-500">Note List</h3>
+  <section class="h-screen bg-pink-50">
+    <header class="bg-pink-100">
+      <div class=" py-5 w-[90%] mx-auto">
+        <div class="text-2xl text-pink-500">Spring Note</div>
+      </div>
+    </header>
+    <main class="grid gap-4 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 w-[90%] mx-auto mt-5" v-if="notes.length > 0">
+      <NoteCard
+          v-for="note in notes"
+          :key="note.id"
+          :note="note"
+      />
+    </main>
+    <main class="m-10 flex items-center justify-center" v-else>
+      <h3 class="text-2xl text-pink-700">No notes yet.</h3>
+    </main>
   </section>
 </template>
---
+++ client/src/views/home/note-list/components/NoteCard.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+
+defineProps<{
+  note: {
+    id: number,
+    title: string,
+    caption: string,
+    createDate: string
+  }
+}>()
+
+function formatDate(date: string) {
+  return new Date(date).toLocaleDateString()
+}
+</script>
+
+<template>
+  <div class="border-pink-500 border rounded p-4 shadow-lg hover:shadow-pink-300 cursor-pointer hover:-translate-y-1">
+    <h3 class="text-lg text-pink-800 line-clamp-1">{{ note.title }}</h3>
+    <hr class="text-pink-500 py-2">
+    <p class="mb-3 text-pink-700 line-clamp-2">{{ note.caption }}</p>
+    <span class="text-sm text-pink-400 line-clamp-1">{{ formatDate(note.createDate) }}</span>
+  </div>
+</template>
